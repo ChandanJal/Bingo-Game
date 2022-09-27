42149232df15ad328db18e357a5955ec4c80bcff
Chandan Jal <chandan.jal.code@gmail.com>
2022-09-27 21:00:29 +0530

    Bottom button added

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,78 @@
-
+// Variables
 const boxContainer = document.querySelector("[data-box-container]");
+const numberContainer = document.querySelector("[data-number-container]");
+const numbers = document.querySelectorAll(".number");
+
+// Initialize
+init();
+
+// Events
+
+// Functions
+function init() {
+  const nums = getNumberBoxes();
+  for (let i = 0; i < nums.length; i++) {
+    nums[i].textContent = i + 1;
+    nums[i].dataset.key = i + 1;
+  }
+
+  numbers.forEach((n) => {
+    n.addEventListener("click", () => pressKey(n.dataset.key));
+  });
+}
 
 function startInteraction() {
-    document.addEventListener("keydown", handleKeyPress);
+  document.addEventListener("keydown", handleKeyPress);
+}
+
+function stopInteraction() {
+  document.removeEventListener("keydown", handleKeyPress);
+}
+
+function randomGenerate() {
+    
 }
 
 function pressKey(key) {
-    const nextBox = boxContainer.querySelector(":not([data-letter])");
-    if (nextBox) {
-        nextBox.dataset.letter = key.toLowerCase();
-        nextBox.textContent = key;
-        nextBox.dataset.state = "active";
-    }
+  const boxes = Array.from(getFilledBoxes());
+  if (boxes.findIndex((v) => v.dataset.letter === key.toLowerCase()) > -1) {
+    // already exist
+    return;
+  }
+
+  const nextBox = boxContainer.querySelector(":not([data-letter])");
+  if (nextBox) {
+    nextBox.dataset.letter = key.toLowerCase();
+    nextBox.textContent = key;
+    nextBox.dataset.state = "active";
+    numberContainer.removeChild(numberContainer.querySelector(`[data-key="${key}"]`));
+  }
 }
 
 function handleKeyPress(e) {
-    if (e.key.match(/^\d$/)) return pressKey(e.key);
+  if (e.key === "Enter") return console.log("Clicked Enter");
+
+  if (e.key === "Backspace") return removeLastElement();
+
+  if (e.key.match(/^\d$/)) return pressKey(e.key);
 }
 
+function removeLastElement() {
+  const boxes = Array.from(getFilledBoxes());
+  if (boxes.length === 0) return;
 
-startInteraction()
+  const lastBox = boxes[boxes.length - 1];
+  delete lastBox.dataset.letter;
+  delete lastBox.dataset.state;
+  lastBox.textContent = "";
+}
+
+function getFilledBoxes() {
+  return boxContainer.querySelectorAll("[data-letter]");
+}
+
+function getNumberBoxes() {
+  return Array.from(numberContainer.querySelectorAll(".number"));
+}
+
+startInteraction();
